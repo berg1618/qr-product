e45864f5339bf1c0cdf637080eff41ebae4ab639
berg1618 <berg@blickdev.com.br>
2024-08-21 10:46:26 -0300

    add mongoose

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const mongoose = require('mongoose');
 
 const port = 3001;
 
@@ -13,6 +16,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(`mongodb+srv://berg-b:${process.env.DB_PASSWORD}@cluster0.j3xh1vt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
